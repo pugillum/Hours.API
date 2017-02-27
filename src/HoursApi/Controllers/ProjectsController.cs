27f589c32ae087604397ddcfe77489edb59b8b37
pugillum <pugillum@gmail.com>
2017-02-27 22:36:38 +0100

    First checkin

---
+++ src/HoursApi/Controllers/ProjectsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HoursApi.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController: Controller
+    {
+        [HttpGet()]
+        public IActionResult GetProjects()
+        {
+            return Ok(ProjectsDataStore.Current.Projects);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProject(int id)
+        {
+            // find city
+            var projectToReturn = ProjectsDataStore.Current.Projects.FirstOrDefault(c => c.Id == id);
+            if (projectToReturn == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(projectToReturn);
+        }
+    }
+}

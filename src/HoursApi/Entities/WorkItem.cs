3c3cc5902eb49c38a7a8d81c5bae54c316638306
pugillum <pugillum@gmail.com>
2017-03-08 22:26:28 +0100

    Added WorkItem class

---
+++ src/HoursApi/Entities/WorkItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HoursApi.Entities
+{
+    public class WorkItem
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime StartTime { get; set; }
+
+        [Required]
+        public DateTime EndTime { get; set; }
+
+        [MaxLength(200)]
+        public string Description { get; set; }
+    }
+}
